416ab5c78c90b7d3a986857cc24273a3964bdcf7
Evan Brodie <evanb@nulogy.com>
2018-11-07 16:50:18 -0500

    Adds cask file for v1.0.0

---
+++ branj.rb
@@ -0,0 +1,10 @@
+cask 'branj' do
+  version '1.0.0'
+  sha256 '0d0bb3e6469e22410cf8aa1d0c9ebb5745525179fd5951c1ba901c548aa6ac23'
+
+  url 'https://github.com/nulogy/branj/archive/1.0.0.zip'
+  name 'branj'
+  homepage 'https://github.com/nulogy/branj'
+
+  binary 'branj'
+end
