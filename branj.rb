f52cbdcea2bdb01ac94cf02e4cba431113463e2d
Evan Brodie <evanb@nulogy.com>
2018-11-08 10:49:18 -0500

    Version bump (1.0.0 --> 1.0.1)

--- branj.rb
+++ branj.rb
@@ -1,8 +1,8 @@
 cask 'branj' do
-  version '1.0.0'
+  version '1.0.1'
   sha256 '0d0bb3e6469e22410cf8aa1d0c9ebb5745525179fd5951c1ba901c548aa6ac23'
 
-  url 'https://github.com/nulogy/branj/archive/1.0.0.zip'
+  url 'https://github.com/nulogy/branj/archive/1.0.1.zip'
   name 'branj'
   homepage 'https://github.com/nulogy/branj'
 
@@ -10,5 +10,5 @@ cask 'branj' do
   depends_on formula: 'git'
   depends_on formula: 'jq'
 
-  binary 'branj-1.0.0/branj'
+  binary 'branj-1.0.1/branj'
 end
